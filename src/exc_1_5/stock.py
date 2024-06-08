1edf379db4691e8727a706a9cdb4a489ae8e92ae
Roberts Scetinins <skenline@inbox.lv>
2024-06-08 20:52:46 +0200

    Add exc 3.1

--- src/exc_1_5/stock.py
+++ src/exc_1_5/stock.py
@@ -1,3 +1,4 @@
+import csv
 
 
 class Stock:
@@ -8,3 +9,24 @@ class Stock:
 
     def cost(self):
         return self.shares * self.price
+
+    def sell(self, shares):
+        self.shares -= shares
+
+
+def read_portfolio(filepath: str) -> list[Stock]:
+    stock_records = []
+    with open(filepath) as file:
+        csv_file = csv.reader(file)
+        next(csv_file)
+        for row in csv_file:
+            stock = Stock(row[0], int(row[1]), float(row[2]))
+            stock_records.append(stock)
+    return stock_records
+
+
+def print_portfolio(stocks: list[Stock]) -> None:
+    print('%10s %10s %10s' % ("name", "shares", "price"))
+    print(('-'*10 + ' ')*3)
+    for stock in stocks:
+        print(f"{stock.name:>10} {stock.shares:10d} {stock.price:10.2f}")
